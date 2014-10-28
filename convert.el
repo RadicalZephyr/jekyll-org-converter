8bc87561c72023e87628597a43c63bb6fe7a08f2
Geoff Shannon <geoffpshannon@gmail.com>
2014-10-28 13:30:33 -0400

    Condense the lisp a bit

--- convert.el
+++ convert.el
@@ -4,11 +4,5 @@
   (setq org-html-head-include-scripts       nil)
   (condition-case nil (while t (insert (read-string "") "")) (error nil))
   (set-buffer (org-export-to-buffer 'html
-                  "*Org HTML Export*"
-                nil
-                nil
-                nil
-                t
-                nil
-                (lambda nil t)))
+                  "*Org HTML Export*" nil nil nil t nil (lambda nil t)))
   (message "%s" (buffer-string)))
