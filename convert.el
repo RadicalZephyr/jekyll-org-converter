656e0a5087a02e7e637a47d4c10cb16692720e06
Geoff Shannon <geoffpshannon@gmail.com>
2014-10-28 13:22:40 -0400

    Change the relevant org-mod variables

--- convert.el
+++ convert.el
@@ -1,7 +1,8 @@
 (progn
   (require 'org)
-  (condition-case nil (while t (insert (read-string "") "
-")) (error nil))
+  (setq org-html-head-include-default-style nil)
+  (setq org-html-head-include-scripts       nil)
+  (condition-case nil (while t (insert (read-string "") "")) (error nil))
   (set-buffer (org-export-to-buffer 'html
                   "*Org HTML Export*"
                 nil
