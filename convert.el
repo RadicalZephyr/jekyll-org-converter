70b3a6e562bd7498eab3c11a0460ee9ebf4f925f
Geoff Shannon <geoffpshannon@gmail.com>
2014-10-28 14:23:45 -0400

    Critically correct the insert function to preserve newlines

--- convert.el
+++ convert.el
@@ -2,7 +2,7 @@
   (require 'org)
   (setq org-html-head-include-default-style nil)
   (setq org-html-head-include-scripts       nil)
-  (condition-case nil (while t (insert (read-string "") "")) (error nil))
+  (condition-case nil (while t (insert (read-string "") "\n")) (error nil))
   (set-buffer (org-export-to-buffer 'html
                   "*Org HTML Export*" nil nil nil t nil (lambda nil t)))
   (message "%s" (buffer-string)))
